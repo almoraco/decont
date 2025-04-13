9aed0db1ca44cb833bf44d90d38b843231a0b240
Alfonso Mora <amorac@cnio.es>
2025-04-13 12:03:45 +0200

    Creación de un log de la pipeline
    LOG_FILE="pipeline_log.txt"

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -2,7 +2,20 @@
 # Script para el curso de bioinformática
 # que "descontamina" los datos de un experimentos de small-RNA secuqncing
 
-#last updated: 13-04-2025 10:18
+#last updated: 13-04-2025 11:58
+
+
+# Definir archivo de log para laa pipeline
+# Se creará en el directorio principal
+LOG_FILE="pipeline_log.txt"
+
+# Función para registrar mensajes en el archivo de log
+log_message() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
+    echo "$1"
+}
+
+log_message "==== Iniciando pipeline de procesamiento ===="
 
 #Descarga de los archivos escritos en data/urls
 # comprobando que no existen ya
@@ -23,16 +36,19 @@ done < data/urls
 
 # Download the contaminants fasta file, uncompress it, and
 # filter to remove all small nuclear RNAs
-
+log_message "Descargando los archivos fasta de los contaminantes"
 URL="https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz"
 UNCOMPRESS_OPTION="yes"
 EXCLUDE_WORD="small nuclear RNA" # para excluir small nuclear RNAs
        
 bash scripts/download.sh "$URL" res "$UNCOMPRESS_OPTION" "$EXCLUDE_WORD"
-
+log_message "Archivo de contaminantes descargado, descomprimido y filtrado"
 
 # Index the contaminants file
+log_message "Indexando archivo de contaminantes"
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
+log_message "Indexado completado exitosamente"
+
 
 # Merge the samples into a single file
 for sid in $(<list_of_sample_ids>) #TODO
@@ -41,18 +57,18 @@ do
 done
 
 # TODO: run cutadapt for all merged files
-# cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
-#     -o <trimmed_file> <input_file> > <log_file>
+cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
+     -o <trimmed_file> <input_file> > <log_file>
 
 # TODO: run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
     sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-    #    --outReadsUnmapped Fastx --readFilesIn <input_file> \
-    #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
+    mkdir -p out/star/$sid
+    STAR --runThreadN 6 --genomeDir res/contaminants_idx \
+        --outReadsUnmapped Fastx --readFilesIn <input_file> \
+        --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
 done 
 
 # TODO: create a log file containing information from cutadapt and star logs
